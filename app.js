fa9fe1589f545674fb9be73eca2f46980877ea8c
johannes <johannes@oarsnet.nl>
2026-06-27 14:42:10 +0200

    app modifications

--- app.js
+++ app.js
@@ -1,55 +1,51 @@
 import express from 'express';
 import { jsonData } from './data.js';
 import { initializeCache, getPages, getNav } from './lib/cacheManager.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const PORT = 3000;
 
 await initializeCache();
-const navigation = {hoofdmenu: getNav().navMenus?.hoofdmenu, footerMenu: getNav().navMenus['footer-menu-personal-pages']}
+const navigation = { hoofdmenu: getNav().navMenus?.hoofdmenu, footerMenu: getNav().navMenus['footer-menu-personal-pages'] }
+
+// Recreating __dirname for ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+app.use(express.static(path.join(__dirname, 'public'), { index: false }));
+
 app.get('/', async (req, res) => {
     try {
-        const navMenus = getNav().navMenus;
-        const filteredPost = getPages()?.find(item => item['wp:post_id'] == 508)
-        
-        const post = {
-                title: filteredPost['title'],
-                content: filteredPost['content:encoded'],
-            };
+        const filteredPage = getPages()?.find(item => item['wp:post_id'] == 508)
+        const page = {
+            content: filteredPage['content:encoded'],
+            date: filteredPage['wp:post_date']
+        };
 
-            res.render('frontpage', { post:post, ...navigation });
+        res.render('frontpage', { page: page, ...navigation });
     } catch (error) {
         console.error('Error fetching data:', error);
         res.status(500).json({ error: 'Failed to load data' });
     }
 });
 
-app.get('/menu', async (req, res) => {
-    try {
-            const navMenus = getNav().navMenus['footer-menu-personal-pages'];
-            res.render('menu', { hoofdmenu:navMenus.hoofdmenu });
-        } catch (error) {
-            console.error('Error fetching data:', error);
-            res.status(500).json({ error: 'Failed to load data' });
-        }
-    });
-
 app.get('/:post', async (req, res) => {
     try {
-        const page = getPages()?.find(item => item['wp:post_name'] == req.params.post);
+        const filteredPage = getPages()?.find(item => item['wp:post_name'] == req.params.post);
 
-        if (page) {
-            const mappedPage = {
-                title: page['title'],
-                content: page['content:encoded'],
-                date: page['wp:post_date'],
-                author: page['dc:creator']
+        if (filteredPage) {
+            const page = {
+                title: filteredPage['title'],
+                content: filteredPage['content:encoded'],
+                date: filteredPage['wp:post_date'],
+                author: filteredPage['dc:creator']
             };
-            res.render('page', { page: mappedPage, ...navigation });
+            res.render('page', { page: page, ...navigation });
         } else {
             res.status(404).json({ error: 'Post not found' });
         }
